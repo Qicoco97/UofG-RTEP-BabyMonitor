27bc81170b02e30e879d0e7d3ac4baa623b2af43
George Winsdor <439375996@qq.com>
2025-07-06 18:57:35 +0100

    add notation

--- src/GUI/mainwindow.cpp
+++ src/GUI/mainwindow.cpp
@@ -1,18 +1,3 @@
-//#include "mainwindow.h"
-//#include "./ui_mainwindow.h"
-
-//MainWindow::MainWindow(QWidget *parent)
-//    : QMainWindow(parent)
-//    , ui(new Ui::MainWindow)
-//{
-//    ui->setupUi(this);
-//}
-
-//MainWindow::~MainWindow()
-//{
-//    delete ui;
-//}
-
 // MainWindow.cpp
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
